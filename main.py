0f4580f1555983253fb5becab767ae6e2ddded37
Demiart <arteomrusakov@mail.ru>
2024-02-27 02:56:53 +0900

    готовое главное меню. реадлизация системы уровней и инициализации всего внутри игрового цикла

--- main.py
+++ main.py
@@ -14,26 +14,28 @@ clock = pygame.time.Clock()
 fps = 60
 
 
-screen_width = 704  # ширина
-screen_height = 704  # высота
+SCREEN_WIDTH = 704  # ширина
+SCREEN_HEIGHT = 704  # высота
 
-screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption('Air Battles')
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+TEXT_COL = (200, 200, 200)
 
 
 # определение шрифтов
+font = pygame.font.SysFont("arialblack", 40)
 font30 = pygame.font.SysFont('Arial', 30)
 font40 = pygame.font.SysFont('Arial', 40)
 
 
 # загрузка звука
-explosion_fx = pygame.mixer.Sound("img/explosion.wav")
+explosion_fx = pygame.mixer.Sound("img/explosion.wav")  # звук взрыва
 explosion_fx.set_volume(0.25)  # громкость
 
-explosion2_fx = pygame.mixer.Sound("img/explosion2.wav")
+explosion2_fx = pygame.mixer.Sound("img/explosion2.wav")  # звук взрыва
 explosion2_fx.set_volume(0.25)
 
-laser_fx = pygame.mixer.Sound("img/laser.wav")
+laser_fx = pygame.mixer.Sound("img/laser.wav")  # звук выстрела
 laser_fx.set_volume(0.25)
 
 
@@ -45,6 +47,8 @@ last_alien_shot = pygame.time.get_ticks()
 countdown = 3  # обратный отсчёт
 last_count = pygame.time.get_ticks()
 game_over = 0  # тригер победы или поражения
+hp_aliens = 1  # очки здоровья врагов
+
 
 # цвета
 red = (255, 0, 0)
@@ -54,9 +58,10 @@ white = (255, 255, 255)
 
 # установка фото
 bg = pygame.image.load("img/bg.png")  # задний фон
+main_fon = pygame.image.load('img/main_fon.png')
 
 
-def draw_bg():
+def draw_bg(bg):
 	screen.blit(bg, (0, 0))
 
 
@@ -64,6 +69,35 @@ def draw_bg():
 def draw_text(text, font, text_col, x, y):
 	img = font.render(text, True, text_col)
 	screen.blit(img, (x, y))
+
+
+class Button():
+	def __init__(self, x, y, image, scale):
+		width = image.get_width()
+		height = image.get_height()
+		self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
+		self.rect = self.image.get_rect()
+		self.rect.topleft = (x, y)
+		self.clicked = False
+
+	def draw(self, surface):
+		action = False
+		#get mouse position
+		pos = pygame.mouse.get_pos()
+
+		#check mouseover and clicked conditions
+		if self.rect.collidepoint(pos):
+			if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+				self.clicked = True
+				action = True
+
+		if pygame.mouse.get_pressed()[0] == 0:
+			self.clicked = False
+
+		#draw button on screen
+		surface.blit(self.image, (self.rect.x, self.rect.y))
+
+		return action
 
 
 # класс самолёта
@@ -84,20 +118,19 @@ class MainAirplane(pygame.sprite.Sprite):
 		# скорость перемещения
 		speed = 8
 		# откат пули
-		cooldown = 500  #  милисекунды
+		cooldown = 500  # милисекунды
 		game_over = 0
-		ability_cooldawn = 10000
 
 
 		# получаем нажатие клавиши
 		key = pygame.key.get_pressed()
 		if key[pygame.K_LEFT] and self.rect.left > 0:
 			self.rect.x -= speed
-		if key[pygame.K_RIGHT] and self.rect.right < screen_width:
+		if key[pygame.K_RIGHT] and self.rect.right < SCREEN_WIDTH:
 			self.rect.x += speed
 		if key[pygame.K_UP] and self.rect.top > 0:
 			self.rect.y -= speed
-		if key[pygame.K_DOWN] and self.rect.bottom < screen_height:
+		if key[pygame.K_DOWN] and self.rect.bottom < SCREEN_HEIGHT:
 			self.rect.y += speed
 
 		# запись текущего времени
@@ -110,10 +143,12 @@ class MainAirplane(pygame.sprite.Sprite):
 			bullet_group.add(bullet)
 			self.last_shot = time_now
 
-		# откат ульты
-		# использование ульты
-
-
+		# проверкаа пересечения с врагом
+		if pygame.sprite.spritecollide(self, alien_group, True):
+			self.health_remaining -= 1
+			explosion_fx.play()
+			explosion = Explosion(self.rect.centerx, self.rect.centery, 2)
+			explosion_group.add(explosion)
 
 		# обновить маску
 		self.mask = pygame.mask.from_surface(self.image)
@@ -187,12 +222,12 @@ class Alien_Bullets(pygame.sprite.Sprite):
 
 	def update(self):
 		self.rect.y += 2
-		if self.rect.top > screen_height:
+		if self.rect.top > SCREEN_HEIGHT:
 			self.kill()
 		if pygame.sprite.spritecollide(self, spaceship_group, False, pygame.sprite.collide_mask):
 			self.kill()
 			explosion2_fx.play()
-			#reduce spaceship health
+			# уменьшить хп корабля
 			spaceship.health_remaining -= 1
 			explosion = Explosion(self.rect.centerx, self.rect.centery, 1)
 			explosion_group.add(explosion)
@@ -221,7 +256,6 @@ class Explosion(pygame.sprite.Sprite):
 		self.rect.center = [x, y]
 		self.counter = 0
 
-
 	def update(self):
 		explosion_speed = 5
 		# обновление анимации взрыва
@@ -235,8 +269,6 @@ class Explosion(pygame.sprite.Sprite):
 		# удалить взрыв если анимация закончилась
 		if self.index >= len(self.images) - 1 and self.counter >= explosion_speed:
 			self.kill()
-
-
 
 
 # группы спрайтов
@@ -254,78 +286,119 @@ def create_aliens():
 			alien = Aliens(150 + item * 100, 100 + row * 70)
 			alien_group.add(alien)
 
-create_aliens()
-
 
 # создание персонажа
-spaceship = MainAirplane(int(screen_width / 2), screen_height - 100, 3)
-spaceship_group.add(spaceship)
+spaceship = MainAirplane(int(SCREEN_WIDTH / 2), SCREEN_HEIGHT - 100, 3)
 
+
+# пределение фотографий кнопок
+resume_img = pygame.image.load("img/button_resume.png").convert_alpha()  # кнопка продолжения
+quit_img = pygame.image.load("img/button_quit.png").convert_alpha()  # кнопка выхода
+keys_img = pygame.image.load('img/button_keys.png').convert_alpha()  # кнопка биндов
+back_img = pygame.image.load('img/button_back.png').convert_alpha()  # кнопка возвращения
+
+resume_button = Button(260, 125, resume_img, 1)  # кнопка продолжения
+quit_button = Button(290, 475, quit_img, 1)  # кнопка выхода
+keys_button = Button(200, 300, keys_img, 1)  # кнопка биндов
+back_button = Button(290, 550, back_img, 1)  # кнопка возвращения
+
+game_paused = True  # статус паузы
+menu_state = "main"  # статус меню
 
 
 run = True
 while run:
+	# рисовка задника
 
 	clock.tick(fps)
+	if game_paused:
+		draw_bg(main_fon)
+		# состояние меню
+		if menu_state == 'main':
+			# экранные кнопки паузы
+			if resume_button.draw(screen):
+				game_paused = False
+				create_aliens()
+				spaceship_group.add(spaceship)
+			if quit_button.draw(screen):
+				run = False
+			if keys_button.draw(screen):
+				menu_state = 'keys'
+		if menu_state == 'keys':
+			draw_text('Движение героя - стрелки', font, TEXT_COL, 50, 100)
+			draw_text('Пауза - Esc', font, TEXT_COL, 230, 250)
+			draw_text('Стрельба - space', font, TEXT_COL, 180, 400)
+			if back_button.draw(screen):
+				menu_state = 'main'
+	else:
+		draw_bg(bg)
+		menu_state = 0
 
-	#draw background
-	draw_bg()
+		# инициализация всех действующих объектов
+
+		if countdown == 0:
+			# создание пуль из рандомного врага
+			# запись текущего времени
+			time_now = pygame.time.get_ticks()
+			# стрелять
+			if time_now - last_alien_shot > alien_cooldown and len(alien_bullet_group) < 5 and len(alien_group) > 0:
+				attacking_alien = random.choice(alien_group.sprites())
+				alien_bullet = Alien_Bullets(attacking_alien.rect.centerx, attacking_alien.rect.bottom)
+				alien_bullet_group.add(alien_bullet)
+				last_alien_shot = time_now
+
+			# проверка убиты ли все пришельци
+			if len(alien_group) == 0:
+				game_over = 1
+
+			if game_over == 0:
+				# обновление гг
+				game_over = spaceship.update()
+
+				# обновление групп спрайтов
+				bullet_group.update()
+				alien_group.update()
+				alien_bullet_group.update()
+			else:
+				if game_over == -1:
+					draw_text('GAME OVER!', font40, white, int(SCREEN_WIDTH / 2 - 100), int(SCREEN_HEIGHT / 2 + 50))
+					if back_button.draw(screen):
+						game_paused = True
+						menu_state = 'main'
+						countdown = 3
+						game_over = 0
+				if game_over == 1:
+					draw_text('YOU WIN!', font40, white, int(SCREEN_WIDTH / 2 - 100), int(SCREEN_HEIGHT / 2 + 50))
+					if back_button.draw(screen):
+						menu_state = 'main'
+						game_paused = True
+						countdown = 3
+						game_over = 0
 
 
-	if countdown == 0:
-		#create random alien bullets
-		#record current time
-		time_now = pygame.time.get_ticks()
-		#shoot
-		if time_now - last_alien_shot > alien_cooldown and len(alien_bullet_group) < 5 and len(alien_group) > 0:
-			attacking_alien = random.choice(alien_group.sprites())
-			alien_bullet = Alien_Bullets(attacking_alien.rect.centerx, attacking_alien.rect.bottom)
-			alien_bullet_group.add(alien_bullet)
-			last_alien_shot = time_now
+		if countdown > 0:
+			draw_text('GET READY!', font40, white, int(SCREEN_WIDTH / 2 - 110), int(SCREEN_HEIGHT / 2 + 50))
+			draw_text(str(countdown), font40, white, int(SCREEN_WIDTH / 2 - 10), int(SCREEN_HEIGHT / 2 + 100))
+			count_timer = pygame.time.get_ticks()
+			if count_timer - last_count > 1000:
+				countdown -= 1
+				last_count = count_timer
 
-		#check if all the aliens have been killed
-		if len(alien_group) == 0:
-			game_over = 1
+		# обновить взрывную группу
+		explosion_group.update()
 
-		if game_over == 0:
-			#update spaceship
-			game_over = spaceship.update()
+		# нарисуйте группы спрайтов
+		spaceship_group.draw(screen)
+		bullet_group.draw(screen)
+		alien_group.draw(screen)
+		alien_bullet_group.draw(screen)
+		explosion_group.draw(screen)
 
-			#update sprite groups
-			bullet_group.update()
-			alien_group.update()
-			alien_bullet_group.update()
-		else:
-			if game_over == -1:
-				draw_text('GAME OVER!', font40, white, int(screen_width / 2 - 100), int(screen_height / 2 + 50))
-			if game_over == 1:
-				draw_text('YOU WIN!', font40, white, int(screen_width / 2 - 100), int(screen_height / 2 + 50))
-
-	if countdown > 0:
-		draw_text('GET READY!', font40, white, int(screen_width / 2 - 110), int(screen_height / 2 + 50))
-		draw_text(str(countdown), font40, white, int(screen_width / 2 - 10), int(screen_height / 2 + 100))
-		count_timer = pygame.time.get_ticks()
-		if count_timer - last_count > 1000:
-			countdown -= 1
-			last_count = count_timer
-
-
-	#update explosion group
-	explosion_group.update()
-
-
-	#draw sprite groups
-	spaceship_group.draw(screen)
-	bullet_group.draw(screen)
-	alien_group.draw(screen)
-	alien_bullet_group.draw(screen)
-	explosion_group.draw(screen)
-
-
-	#event handlers
+	# обработчики событий
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			run = False
+			menu_state = 'main'
 
 
 	pygame.display.update()
